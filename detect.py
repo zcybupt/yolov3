bd1b9032c5cf4b733f31e75fa5d5f89aa59cf6cf
“zcybupt” <jp2016213431@qmul.ac.uk>
2019-12-09 11:24:50 +0800

    single img detect

--- detect.py
+++ detect.py
@@ -4,9 +4,10 @@ from sys import platform
 from models import *  # set ONNX_EXPORT in models.py
 from utils.datasets import *
 from utils.utils import *
+import cv2
 
 
-def detect(save_txt=False, save_img=False):
+def detect(filepath, save_txt=False, save_img=False):
     img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
     out, source, weights, half, view_img = opt.output, opt.source, opt.weights, opt.half, opt.view_img
     webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
@@ -74,92 +75,68 @@ def detect(save_txt=False, save_img=False):
     # Run inference
     t0 = time.time()
     count = 0
-    for path, img, im0s, vid_cap in dataset:
-        t = time.time()
+    # for path, img, im0s, vid_cap in dataset:
+    img0 = cv2.imread(filepath)
+    if img0 is None:
+        return
 
-        # Get detections
-        img = torch.from_numpy(img).to(device)
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
-        pred = model(img)[0]
+    # Padded resize
+    img = letterbox(img0, new_shape=img_size)[0]
 
-        if opt.half:
-            pred = pred.float()
+    # Normalize RGB
+    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
+    img = np.ascontiguousarray(img, dtype=np.float16 if half else np.float32)  # uint8 to fp16/fp32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
 
-        # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.nms_thres)
+    t = time.time()
 
-        # Apply
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
+    # Get detections
+    img = torch.from_numpy(img).to(device)
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    pred = model(img)[0]
 
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0 = path[i], '%g: ' % i, im0s[i]
-            else:
-                p, s, im0 = path, '', im0s
+    if opt.half:
+        pred = pred.float()
 
-            save_path = str(Path(out) / Path(p).name)
-            s += '%gx%g ' % img.shape[2:]  # print string
-            if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+    # Apply NMS
+    pred = non_max_suppression(pred, opt.conf_thres, opt.nms_thres)
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += '%g %ss, ' % (n, classes[int(c)])  # add to string
+    # Apply
+    if classify:
+        pred = apply_classifier(pred, modelc, img, img0)
 
-                # Write results
-                for *xyxy, conf, _, cls in det:
-                    if save_txt:  # Write to file
-                        with open(save_path + '.txt', 'a') as file:
-                            file.write(('%g ' * 6 + '\n') % (*xyxy, cls, conf))
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        s, im0 = '', img0
 
-                    if save_img or view_img:  # Add bbox to image
-                        label = '%s %.2f' % (classes[int(cls)], conf)
-                        # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
+        s += '%gx%g ' % img.shape[2:]  # print string
+        if det is not None and len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-            print('%sDone. (%.3fs)' % (s, time.time() - t))
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += '%g %ss, ' % (n, classes[int(c)])  # add to string
 
-            # Stream results
-            if view_img:
-                cv2.imshow(p, im0)
+        print('%sDone. (%.3fs)' % (s, time.time() - t))
 
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'images':
-                    if pred[0] is not None:
-                        watermaks = np.asarray(pred[0])
-                        for watermak in watermaks:
-                            results = [int(i) for i in watermak[:4]]
-                            print(results, watermak[4:].tolist())
-                            save_path = 'output/' + str(count).zfill(5) + '.jpg'
-                            count += 1
-                            cv2.imwrite(
-                                save_path,
-                                im0[int(watermak[1]):int(watermak[3]),
-                                int(watermak[0]):int(watermak[2])]
-                            )
-                            cv2.imwrite(save_path, im0)
-                else:
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
-
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
-                    vid_writer.write(im0)
-
-    if save_txt or save_img:
-        print('Results saved to %s' % os.getcwd() + os.sep + out)
-        if platform == 'darwin':  # MacOS
-            os.system('open ' + out + ' ' + save_path)
-
+        # Save results (image with detections)
+        if dataset.mode == 'images':
+            if pred[0] is not None:
+                watermaks = np.asarray(pred[0])
+                for watermak in watermaks:
+                    results = [int(i) for i in watermak[:4]]
+                    print(results, watermak[4:].tolist())
+                    # save_path = 'output/' + str(count).zfill(5) + '.jpg'
+                    # count += 1
+                    # cv2.imwrite(
+                    #     save_path,
+                    #     im0[int(watermak[1]):int(watermak[3]),
+                    #     int(watermak[0]):int(watermak[2])]
+                    # )
+                    # cv2.imwrite(save_path, im0)
     print('Done. (%.3fs)' % (time.time() - t0))
 
 
@@ -181,4 +158,4 @@ if __name__ == '__main__':
     opt = parser.parse_args()
 
     with torch.no_grad():
-        detect()
+        detect('/Users/zcy/Downloads/20190928/15328496_0002.jpg')
