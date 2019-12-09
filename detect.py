881630f742e19cf4070dc0a381af4d8637abcf97
“zcybupt” <jp2016213431@qmul.ac.uk>
2019-12-09 12:35:30 +0800

    single img detect

--- detect.py
+++ detect.py
@@ -7,123 +7,113 @@ from utils.utils import *
 import cv2
 
 
-def detect(filepath, save_txt=False, save_img=False):
-    img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
-    out, source, weights, half, view_img = opt.output, opt.source, opt.weights, opt.half, opt.view_img
-    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
+class Detector:
+    def __init__(self, opt):
+        self.img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (h, w)
+        out, source, weights, self.half, view_img = opt.output, opt.source, opt.weights, opt.half, opt.view_img
 
-    # Initialize
-    device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
-    if os.path.exists(out):
-        shutil.rmtree(out)  # delete output folder
-    os.makedirs(out)  # make new output folder
+        # Initialize
+        self.device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
+        if os.path.exists(out):
+            shutil.rmtree(out)  # delete output folder
+        os.makedirs(out)  # make new output folder
 
-    # Initialize model
-    model = Darknet(opt.cfg, img_size)
+        # Initialize model
+        self.model = Darknet(opt.cfg, self.img_size)
 
-    # Load weights
-    attempt_download(weights)
-    if weights.endswith('.pt'):  # pytorch format
-        model.load_state_dict(torch.load(weights, map_location=device)['model'])
-    else:  # darknet format
-        _ = load_darknet_weights(model, weights)
+        # Load weights
+        attempt_download(weights)
+        if weights.endswith('.pt'):  # pytorch format
+            self.model.load_state_dict(torch.load(weights, map_location=self.device)['model'])
+        else:  # darknet format
+            _ = load_darknet_weights(self.model, weights)
 
-    # Second-stage classifier
-    classify = False
-    if classify:
-        modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
-        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
-        modelc.to(device).eval()
+    def detect(self, filepath):
+        # Second-stage classifier
+        classify = False
+        if classify:
+            modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
+            modelc.load_state_dict(
+                torch.load('weights/resnet101.pt', map_location=self.device)['model'])  # load weights
+            modelc.to(self.device).eval()
 
-    # Fuse Conv2d + BatchNorm2d layers
-    # model.fuse()
+        # Fuse Conv2d + BatchNorm2d layers
+        # model.fuse()
 
-    # Eval mode
-    model.to(device).eval()
+        # Eval mode
+        self.model.to(self.device).eval()
 
-    # Export mode
-    if ONNX_EXPORT:
-        img = torch.zeros((1, 3) + img_size)  # (1, 3, 320, 192)
-        torch.onnx.export(model, img, 'weights/export.onnx', verbose=False, opset_version=11)
+        # Export mode
+        if ONNX_EXPORT:
+            img = torch.zeros((1, 3) + self.img_size)  # (1, 3, 320, 192)
+            torch.onnx.export(self.model, img, 'weights/export.onnx', verbose=False, opset_version=11)
 
-        # Validate exported model
-        import onnx
-        model = onnx.load('weights/export.onnx')  # Load the ONNX model
-        onnx.checker.check_model(model)  # Check that the IR is well formed
-        print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
-        return
+            # Validate exported model
+            import onnx
+            model = onnx.load('weights/export.onnx')  # Load the ONNX model
+            onnx.checker.check_model(model)  # Check that the IR is well formed
+            print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
+            return
 
-    # Half precision
-    half = half and device.type != 'cpu'  # half precision only supported on CUDA
-    if half:
-        model.half()
+        # Half precision
+        half = self.half and self.device.type != 'cpu'  # half precision only supported on CUDA
+        if half:
+            self.model.half()
 
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = True
-        torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=img_size, half=half)
-    else:
-        save_img = True
-        dataset = LoadImages(source, img_size=img_size, half=half)
+        # Get classes and colors
+        classes = load_classes(parse_data_cfg(opt.data)['names'])
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(classes))]
 
-    # Get classes and colors
-    classes = load_classes(parse_data_cfg(opt.data)['names'])
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(classes))]
+        # Run inference
+        t0 = time.time()
+        img0 = cv2.imread(filepath)
+        if img0 is None:
+            return
 
-    # Run inference
-    t0 = time.time()
-    count = 0
-    # for path, img, im0s, vid_cap in dataset:
-    img0 = cv2.imread(filepath)
-    if img0 is None:
-        return
+        # Padded resize
+        img = letterbox(img0, new_shape=self.img_size)[0]
 
-    # Padded resize
-    img = letterbox(img0, new_shape=img_size)[0]
+        # Normalize RGB
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
+        img = np.ascontiguousarray(img, dtype=np.float16 if half else np.float32)  # uint8 to fp16/fp32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
 
-    # Normalize RGB
-    img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
-    img = np.ascontiguousarray(img, dtype=np.float16 if half else np.float32)  # uint8 to fp16/fp32
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        t = time.time()
 
-    t = time.time()
+        # Get detections
+        img = torch.from_numpy(img).to(self.device)
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        pred = self.model(img)[0]
 
-    # Get detections
-    img = torch.from_numpy(img).to(device)
-    if img.ndimension() == 3:
-        img = img.unsqueeze(0)
-    pred = model(img)[0]
+        if opt.half:
+            pred = pred.float()
 
-    if opt.half:
-        pred = pred.float()
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.nms_thres)
 
-    # Apply NMS
-    pred = non_max_suppression(pred, opt.conf_thres, opt.nms_thres)
+        # Apply
+        if classify:
+            pred = apply_classifier(pred, modelc, img, img0)
 
-    # Apply
-    if classify:
-        pred = apply_classifier(pred, modelc, img, img0)
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            s, im0 = '', img0
 
-    # Process detections
-    for i, det in enumerate(pred):  # detections per image
-        s, im0 = '', img0
+            s += '%gx%g ' % img.shape[2:]  # print string
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-        s += '%gx%g ' % img.shape[2:]  # print string
-        if det is not None and len(det):
-            # Rescale boxes from img_size to im0 size
-            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, classes[int(c)])  # add to string
 
-            # Print results
-            for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                s += '%g %ss, ' % (n, classes[int(c)])  # add to string
+            print('%sDone. (%.3fs)' % (s, time.time() - t))
 
-        print('%sDone. (%.3fs)' % (s, time.time() - t))
+            # Save results (image with detections)
 
-        # Save results (image with detections)
-        if dataset.mode == 'images':
             if pred[0] is not None:
                 watermaks = np.asarray(pred[0])
                 for watermak in watermaks:
@@ -137,7 +127,7 @@ def detect(filepath, save_txt=False, save_img=False):
                     #     int(watermak[0]):int(watermak[2])]
                     # )
                     # cv2.imwrite(save_path, im0)
-    print('Done. (%.3fs)' % (time.time() - t0))
+        print('Done. (%.3fs)' % (time.time() - t0))
 
 
 if __name__ == '__main__':
@@ -157,5 +147,9 @@ if __name__ == '__main__':
     parser.add_argument('--view-img', action='store_true', help='display results')
     opt = parser.parse_args()
 
+    detector = Detector(opt)
+
     with torch.no_grad():
-        detect('/Users/zcy/Downloads/20190928/15328496_0002.jpg')
+        while True:
+            path = input('Image path: ')
+            detector.detect(path)
